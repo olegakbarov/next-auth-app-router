2fcaf7d82d51c4b38be3acceaa94792fdfa65cce
Venge M2 MBP <olegakbarov@gmail.com>
2024-03-24 18:22:59 -0700

    auth router prefetch-push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useUserInput } from "@/providers/UserInput";
+import { useRouter } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
+
 export default function Home() {
+  const [q, setQ] = useState("");
+  const router = useRouter();
+  const { updateUserInput, startStreaming } = useUserInput();
+
+  const onChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setQ(e.target.value);
+      updateUserInput(e.target.value);
+    },
+    [updateUserInput],
+  );
+
+  const onClick = useCallback(() => {
+    startStreaming();
+    router.push("/search");
+  }, [router, startStreaming]);
+
+  useEffect(() => {
+    router.prefetch("/search");
+  }, [router]);
+
   return (
-    <main className="flex item-center justify-center h-[300px] pt-10">
-      yolo
-    </main>
+    <>
+      <Input value={q} onChange={onChange} />
+      <Button onClick={onClick}>Search</Button>
+    </>
   );
 }
